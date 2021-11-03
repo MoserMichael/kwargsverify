4f79b7ac89fc745ccac379e3279854649504ef16
Michael Moser <moser.michael@gmail.com>
2021-11-03 05:30:45 +0200

    pip install

---
+++ sandbox-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e 
+
+rm -rf tst || true
+mkdir tst
+pushd tst
+# test installation of module in virtual environment
+virtualenv my-venv
+source my-venv/bin/activate
+
+pip3 install kwchecker
+cp ../*.py .
+python3 ./test.py
+
+echo "everything is fine. test passed"
+
+deactivate
+
+#rm -rf my-venv
+
+popd tst
