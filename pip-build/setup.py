b1a93ea6ff0bd12b1022131701a2ea93c9e4d1da
Michael Moser <moser.michael@gmail.com>
2021-11-03 05:48:34 +0200

    version update

--- pip-build/setup.py
+++ pip-build/setup.py
@@ -12,7 +12,7 @@ print("install-requires:", install_requires)
 
 setuptools.setup(
     name = "kwchecker",
-    version = "1.0.1",
+    version = "1.0.2",
     author = "Michael Moser",
     author_email = "moser.michael@gmail.com",
     description = ("A declarative verifier and sanitizer for python kwargs parameters." ),
